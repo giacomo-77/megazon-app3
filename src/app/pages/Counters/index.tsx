44b97b36f849db5966e3ecc1381b66bf035f8f91
Giacomo Grotto <giacomo.grotto@orientatrium.it>
2021-12-21 11:36:11 +0100

    Aggiunto il componente Counters

---
+++ src/app/pages/Counters/index.tsx
@@ -0,0 +1,15 @@
+/**
+ *
+ * Counters
+ *
+ */
+import * as React from 'react';
+import styled from 'styled-components/macro';
+
+interface Props {}
+
+export function Counters(props: Props) {
+  return <Div></Div>;
+}
+
+const Div = styled.div``;
