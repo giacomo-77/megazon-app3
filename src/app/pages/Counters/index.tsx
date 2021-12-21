d63dc6eb36c1fa57a797bcf103960c9353878e03
Giacomo Grotto <giacomo.grotto@orientatrium.it>
2021-12-21 12:13:25 +0100

    Selettore e visualizzazione dei contatori

--- src/app/pages/Counters/index.tsx
+++ src/app/pages/Counters/index.tsx
@@ -4,12 +4,27 @@
  *
  */
 import * as React from 'react';
+import { useSelector } from 'react-redux';
 import styled from 'styled-components/macro';
+import { useCountersSlice } from './slice';
+import { selectCounters } from './slice/selectors';
 
 interface Props {}
 
 export function Counters(props: Props) {
-  return <Div></Div>;
+  let { actions } = useCountersSlice();
+  let counters = useSelector(selectCounters);
+  return (
+    <Div>
+      <ul>
+        {counters.map(counter => (
+          <li key={counter.id}>
+            Counter {counter.id}: {counter.value}
+          </li>
+        ))}
+      </ul>
+    </Div>
+  );
 }
 
 const Div = styled.div``;
